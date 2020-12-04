3dc6dffeb71846c57717cc955c254495100af29d
LGCaparros <57749321+LGCaparros@users.noreply.github.com>
2020-12-04 11:32:46 +0100

    Add files via upload

---
+++ SuperheroesTrigger/Superheroes/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Superheroes
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Superheroe> listaSuperheroes;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            listaSuperheroes = Superheroe.GetSamples();
+            superheroeDockPanel.DataContext = listaSuperheroes.FirstOrDefault();
+            actualTextBlock.Text = "1";
+            totalTextBlock.Text = listaSuperheroes.Count.ToString();
+        }
+
+        private void aceptarButton_Click(object sender, RoutedEventArgs e)
+        {
+            listaSuperheroes.Add((Superheroe)this.Resources["nuevo"]);
+            MessageBox.Show("Superhéroe insertado con exito", "Superhéroes", MessageBoxButton.OK, MessageBoxImage.Information);
+            totalTextBlock.Text = listaSuperheroes.Count.ToString();
+
+            ReiniciarSuperHeroe();
+        }
+
+        private void leftImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            int actual = Int32.Parse(actualTextBlock.Text);
+
+            if (actual > 1)
+            {
+                superheroeDockPanel.DataContext = listaSuperheroes[actual - 2];
+                actualTextBlock.Text = (actual - 1).ToString();
+            }
+        }
+
+        private void rightImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            int actual = Int32.Parse(actualTextBlock.Text);
+
+            if (actual < listaSuperheroes.Count)
+            {
+                superheroeDockPanel.DataContext = listaSuperheroes[actual];
+                actualTextBlock.Text = (actual + 1).ToString();
+            }
+        }
+
+        private void limpiarButton_Click(object sender, RoutedEventArgs e)
+        {
+            ReiniciarSuperHeroe();
+        }
+
+        private void ReiniciarSuperHeroe()
+        {
+            this.Resources.Remove("nuevo");
+            this.Resources.Add("nuevo", new Superheroe() { Heroe = true });
+        }
+    }
+}
